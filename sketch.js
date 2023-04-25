f53208076eb8e573bbf0d661034d07013377fd83
milenabr <74979678+milenabr@users.noreply.github.com>
2023-04-25 20:25:47 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var ship, shipImg, sea, seaImg;
+
+function preload(){
+  shipImg = loadAnimation("ship-1.png","ship-2.png");
+  seaImg =  loadImage("sea.png");
+
+}
+
+function setup(){
+  createCanvas(400,400);
+
+  sea = createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("navio",shipImg);
+  ship.scale=0.25;
+
+}
+
+function draw() {
+  background("blue");
+
+  if (sea.x < 0){
+     sea.x = sea.width/7;
+     }
+
+    drawSprites();
+
+  
+  
+}
